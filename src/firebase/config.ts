6eca0a9b629142d3571d1a3a5879903d02dc584f
SAKTHIVARSHAN S URK23CS1211 <sakthivarshans@karunya.edu.in>
2025-10-07 14:05:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6872103078-a22eb",
+  "appId": "1:920238863379:web:99bf08db00f2bb6338bbe6",
+  "apiKey": "AIzaSyCjdn1tF1mDDb1IvM2tdVGRVioSXxMwDns",
+  "authDomain": "studio-6872103078-a22eb.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "920238863379"
+};
